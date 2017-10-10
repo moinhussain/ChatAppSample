b888093edc46c1826cd1ff90d87fb85ae2872016
Moin <moinh@LAPTOP-M73CUK3C.fios-router.home>
2017-10-10 09:48:29 -0400

    changed mongo queries

--- serverExpress.js
+++ serverExpress.js
@@ -3,17 +3,9 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var urlencodeParser = bodyParser.urlencoded({extended:false});
-
 app.use(urlencodeParser);
-
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/ChatDB');
-
-
-
-
-
-
 var ChatRoom     = require('./app/models/chatroom');
 
 /**
@@ -21,43 +13,36 @@ var ChatRoom     = require('./app/models/chatroom');
  */
 app.post('/initiateChatRoom',function(req, res){
     var senderId = req.body.senderId;
-    var recieverId = req.body.reciverId;
+    var recieverId = req.body.recieverId;
     
-    var roomId = "Room_"+senderId+"_"+recieverId
-     ChatRoom.findById(roomId, function(err, chatRoom) {
-
-			if (err){
-                            res.send(err);
-                        }
-				
-                var message = { userId:req.body.senderId, messageId:  mongoose.Types.ObjectId(), message: req.body.message };
-                chatRoom.messages.push(message);
-                chatRoom.save(function(err) {
-                        if (err){
-                            res.send(err);
-
-                        }else{
-                         $responseObj = {"status":201,"message":"message saved successfulyy"};
-                         res.json(JSON.stringify($responseObj));
-
-                        }
-       
-              });
-
-      })
+    var roomId = "Room_"+senderId+"_"+recieverId;
+    
+    ChatRoom.findOne({ roomId: roomId}, function (err, doc){
+        if (err){
+           res.send(err);
+         }
+      if(!doc){
+        var chatRoom = new ChatRoom({ roomId: roomId, messages:[]});
+        chatRoom.save(function (err) {
+          if (err) return handleError(err);
+          // saved!
+          $responseObj = {"status":201,"message":"Room created successfuly",data:{"roomId":roomId}};
+          res.json(JSON.stringify($responseObj));
+        })  
+      }
+});
+    
+    
  })
 /**
- * Save the messages in given room.
+ * Save the messages in given chat room.
  */
 app.post('/saveMessage',function(req,res){
-    
-    
- 
-
-			if (err){
-                            res.send(err);
-                        }
-				
+        ChatRoom.findOne({ roomId: req.body.roomId}, function (err, chatRoom){
+        
+                if (err){
+                 res.send(err);
+                }		
                 var message = { userId:req.body.senderId, messageId:  mongoose.Types.ObjectId(), message: req.body.message };
                 chatRoom.messages.push(message);
                 chatRoom.save(function(err) {
@@ -72,31 +57,33 @@ app.post('/saveMessage',function(req,res){
        
 	          });
 
-});  
-    
+   });  
+ });    
 
 
 /**
- * Update a message for a given room and message
+ * Update a message for a given roomId and messageId
  */
 app.put('/editMessage/:roomId/:messageId/',function(req,res){
-    
-    	ChatRoom.findById(req.params.messageId, function(err, chatRoom) {
-
-			if (err){
-                             res.send(err);
-                        }
-
-
-			chatRoom.name = req.params.roomId;
-			chatRoom.save(function(err) {
-				if (err)
-					res.send(err);
-
-				res.json({ message: 'Bear updated!' });
-			});
-
-		});
+               
+    var roomId   = req.params.roomId;
+    var messageId = req.params.messageId;
+    var newMessage = req.body.message;
+    ChatRoom.findOneAndUpdate({ "roomId": roomId, "messages.messageId": messageId },
+    { 
+        "$set": {
+            "messages.$": newMessage
+        }
+    },
+    function(err,chat) {
+     if (err){
+       res.send(err);
+     }else{
+          $responseObj = {"status":204,"message":"Updated successfully"};
+          res.send($responseObj);  
+     }
+    }
+);
     
 })
 
@@ -105,17 +92,17 @@ app.put('/editMessage/:roomId/:messageId/',function(req,res){
  */
 app.get('/getMessages/:roomId',function(req,res){
     
-        var messages = [];
-       	ChatRoom.findById(req.params.roomId, function(err, chatRoom) {
-
-			if (err){
+      var messages = [];  
+      var roomId = req.params.roomId;
+      ChatRoom.findOne({ roomId: roomId}, function (err, chatRoom){
+		if (err){
                              res.send(err);
                         }
                messages = chatRoom.messages;
                $responseObj = {"status":200,"data":messages};
-               res.send(JSON.stringify($responseObj));
-			});
-
+               res.send($responseObj);
+   });
+        
 });
   
 
