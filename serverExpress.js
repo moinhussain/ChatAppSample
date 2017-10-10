95a41a1b31683a0d0b80ac594250973bf68edfae
Moin <moinh@LAPTOP-M73CUK3C.fios-router.home>
2017-10-10 07:29:16 -0400

    changed mongo queries

--- serverExpress.js
+++ serverExpress.js
@@ -15,11 +15,44 @@ mongoose.connect('mongodb://localhost/ChatDB');
 
 
 var ChatRoom     = require('./app/models/chatroom');
- 
+
+/**
+ * Initiate a conversation by creating room
+ */
+app.post('/initiateChatRoom',function(req, res){
+    var senderId = req.body.senderId;
+    var recieverId = req.body.reciverId;
+    
+    var roomId = "Room_"+senderId+"_"+recieverId
+     ChatRoom.findById(roomId, function(err, chatRoom) {
+
+			if (err){
+                            res.send(err);
+                        }
+				
+                var message = { userId:req.body.senderId, messageId:  mongoose.Types.ObjectId(), message: req.body.message };
+                chatRoom.messages.push(message);
+                chatRoom.save(function(err) {
+                        if (err){
+                            res.send(err);
+
+                        }else{
+                         $responseObj = {"status":201,"message":"message saved successfulyy"};
+                         res.json(JSON.stringify($responseObj));
+
+                        }
+       
+              });
+
+      })
+ })
+/**
+ * Save the messages in given room.
+ */
 app.post('/saveMessage',function(req,res){
     
     
-    ChatRoom.findById(req.body.roomId, function(err, chatRoom) {
+ 
 
 			if (err){
                             res.send(err);
@@ -39,12 +72,14 @@ app.post('/saveMessage',function(req,res){
        
 	          });
 
-});
-
+});  
     
-})
 
-app.put('/editMessage/:messageId',function(req,res){
+
+/**
+ * Update a message for a given room and message
+ */
+app.put('/editMessage/:roomId/:messageId/',function(req,res){
     
     	ChatRoom.findById(req.params.messageId, function(err, chatRoom) {
 
@@ -53,7 +88,7 @@ app.put('/editMessage/:messageId',function(req,res){
                         }
 
 
-			chatRoom.name = req.body.name;
+			chatRoom.name = req.params.roomId;
 			chatRoom.save(function(err) {
 				if (err)
 					res.send(err);
@@ -63,42 +98,27 @@ app.put('/editMessage/:messageId',function(req,res){
 
 		});
     
-    
-    
-     var result = false;
-    var messageId = req.params.messageId;
-    var senderId = req.body.senderId;
-    var recieverId = req.body.recieverId;
-    
-    /*connect db and save the data*/
-   
-    if(result){
- $responseObj = {"status":201,"message":"message updated successfulyy"};
- res.send(JSON.stringify($responseObj));  
-   } res.send(JSON.stringify(books));
-    
 })
 
-app.post('/getMessages',function(req,res){
-    var messages = [];
-    var result = false;
-    var senderId = req.body.senderId;
-    var recieverId = req.body.recieverId;
+/**
+ * Return all the messages in a room
+ */
+app.get('/getMessages/:roomId',function(req,res){
     
-    /*connect db and save the data*/
-   
-    if(result){
-     $responseObj = {"status":200,"data":messages};
-     res.send(JSON.stringify($responseObj));  
-   } 
-    
-})
+        var messages = [];
+       	ChatRoom.findById(req.params.roomId, function(err, chatRoom) {
 
+			if (err){
+                             res.send(err);
+                        }
+               messages = chatRoom.messages;
+               $responseObj = {"status":200,"data":messages};
+               res.send(JSON.stringify($responseObj));
+			});
 
-
+});
+  
 
 var server = app.listen(8083,function(){
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log(host+"-----"+port);
+ 
 });
